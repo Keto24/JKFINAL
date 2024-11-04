7d26f26722dd1fd43b5f689df5b0116ed511e466
onyangojerry <jerry.onyango.rawlings@gmail.com>
2024-11-03 21:08:43 -0800

    done

--- code/wordalign.sh
+++ code/wordalign.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
-# Compile the Java files located in 'nlp/mt' relative to the 'code' directory
-javac nlp/mt/*.java
+# Change to the code directory (where this script is located)
+cd "$(dirname "$0")"
 
-# Run the WordAligner class with the current directory as classpath
-# The classpath includes '.' (current directory) since the script is in 'code'
+# Compile the Java files in the 'nlp/mt' directory, specifying 'code' as the root for packages
+javac -d . nlp/mt/*.java
+
+# Run the WordAligner class with the full package name and classpath as '.'
 java -Xmx2G -cp . code.nlp.mt.WordAligner "$1" "$2" "$3" "$4"
